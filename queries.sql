f97e01c72ad4aeb79bf646324b73be6aedaa9648
Nuri1977 <nurilacka@gmail.com>
2022-03-21 12:06:09 +0100

    Update query with correct date 2

--- queries.sql
+++ queries.sql
@@ -1,7 +1,7 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * from animals WHERE name LIKE '%mon';
-SELECT name FROM animals WHERE date_of_birth <= '2019-31-12' AND date_of_birth >= '2016-01-01';
+SELECT name FROM animals WHERE date_of_birth <= '2019-12-31' AND date_of_birth >= '2016-01-01';
 SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name = 'Pikachu' OR name = 'Agumon';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
